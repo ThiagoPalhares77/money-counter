f62e298fbc9752107f5a0fb170ad59e694c1a259
Thiago <ps-thiago@hotmail.com>
2022-09-28 18:54:24 -0300

    Feat: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello world</h1>
+      <GlobalStyle />
+
+      <Transactions />
     </ThemeProvider>
   )
 }
