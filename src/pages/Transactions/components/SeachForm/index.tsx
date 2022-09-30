35ab4350b67478a79acf3392c393acf66e09a1d8
Thiago <ps-thiago@hotmail.com>
2022-09-30 18:10:31 -0300

    Feat: ContextSelector

--- src/pages/Transactions/components/SeachForm/index.tsx
+++ src/pages/Transactions/components/SeachForm/index.tsx
@@ -4,7 +4,7 @@ import { SeachFormContainer } from './styles'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -13,7 +13,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    }
+  )
 
   const {
     register,
